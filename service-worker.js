b4f9ab8f383a6858aeb70079c572216666462c6e
matrixGB <32918725+matrixGB@users.noreply.github.com>
2019-02-03 05:24:16 -0500

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/motor-coach-direct/precache-manifest.7f40fa740cea8e2daf80d3183f43bca1.js"
+  "/motor-coach-direct/precache-manifest.a9f03deaa75eecdad1a84922a0d1bb5f.js"
 );
 
 workbox.clientsClaim();
