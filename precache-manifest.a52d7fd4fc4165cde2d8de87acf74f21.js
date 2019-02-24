2a1a41818ae60ad93e1b6a7fa7507bfb31c2e86b
matrixGB <32918725+matrixGB@users.noreply.github.com>
2019-02-23 19:45:58 -0500

    Updates

---
+++ precache-manifest.a52d7fd4fc4165cde2d8de87acf74f21.js
@@ -0,0 +1,258 @@
+self.__precacheManifest = [
+  {
+    "revision": "1d0dc32760d6ae2430104a1eb85923d5",
+    "url": "/motor-coach-direct/static/media/2019 Fleetwood Pace Arrow.1d0dc327.png"
+  },
+  {
+    "revision": "8cb45daff5ed6bf63b88",
+    "url": "/motor-coach-direct/static/css/main.b37ae0d3.chunk.css"
+  },
+  {
+    "revision": "f63ecdff5d2dfafec6ab",
+    "url": "/motor-coach-direct/static/js/1.f63ecdff.chunk.js"
+  },
+  {
+    "revision": "eca59dba73ccc225ee48",
+    "url": "/motor-coach-direct/static/js/runtime~main.eca59dba.js"
+  },
+  {
+    "revision": "b48ab955cb5d55843fd9120698824fa3",
+    "url": "/motor-coach-direct/static/media/white-logo.b48ab955.svg"
+  },
+  {
+    "revision": "fce3ca21fd92ecb8fbe54f5e2436bf08",
+    "url": "/motor-coach-direct/static/media/american eagle 45A.fce3ca21.png"
+  },
+  {
+    "revision": "7e5ce48c4781111362f1b5afe5d7bb01",
+    "url": "/motor-coach-direct/static/media/american eagle 45T.7e5ce48c.png"
+  },
+  {
+    "revision": "b8e7daf8e4f32afcde77b304b3107576",
+    "url": "/motor-coach-direct/static/media/yellow-logo.b8e7daf8.svg"
+  },
+  {
+    "revision": "7b5ed1089ec31ac101ecc0d89678e44c",
+    "url": "/motor-coach-direct/static/media/american_coach_logo.7b5ed108.png"
+  },
+  {
+    "revision": "49d5c81b4d45716f3e539c3f50d240c8",
+    "url": "/motor-coach-direct/static/media/dynamax.49d5c81b.png"
+  },
+  {
+    "revision": "92904f319507e6524091be13e27388e8",
+    "url": "/motor-coach-direct/static/media/EntegraCoach.92904f31.png"
+  },
+  {
+    "revision": "f9ac562377c6564331fd73b736b20a6e",
+    "url": "/motor-coach-direct/static/media/fleetwood.f9ac5623.png"
+  },
+  {
+    "revision": "a570bf2ce6a98b6c07d0ce19be50d2d0",
+    "url": "/motor-coach-direct/static/media/monaco.a570bf2c.png"
+  },
+  {
+    "revision": "e5e5b4346567e5f3bcd330c1d356d9c0",
+    "url": "/motor-coach-direct/static/media/Newmar.e5e5b434.png"
+  },
+  {
+    "revision": "5c6b1a93fe094e4653535635483bdc9c",
+    "url": "/motor-coach-direct/static/media/winnebago.5c6b1a93.gif"
+  },
+  {
+    "revision": "749a902a036ccee328a19edb4e10d359",
+    "url": "/motor-coach-direct/static/media/2019 american dream.749a902a.png"
+  },
+  {
+    "revision": "20961d06b49c90bba9d1131d48fa83d4",
+    "url": "/motor-coach-direct/static/media/american dream 42B.20961d06.png"
+  },
+  {
+    "revision": "d26c768f6dcec29197807340f15650a3",
+    "url": "/motor-coach-direct/static/media/american dream 42Q.d26c768f.png"
+  },
+  {
+    "revision": "7cc26b5a6be98b76431e2e4cba3b6672",
+    "url": "/motor-coach-direct/static/media/american dream 42S.7cc26b5a.png"
+  },
+  {
+    "revision": "a97f21bc6ccba6af2f2307b79d4f445e",
+    "url": "/motor-coach-direct/static/media/american dream 45A.a97f21bc.png"
+  },
+  {
+    "revision": "069ef98c2e3909687cb5b44ca6eba2fe",
+    "url": "/motor-coach-direct/static/media/2019 american eagle.069ef98c.png"
+  },
+  {
+    "revision": "d0c376954a61443d9e8648fc05a7c958",
+    "url": "/motor-coach-direct/static/media/american eagle 45C.d0c37695.png"
+  },
+  {
+    "revision": "254ef606b307181119c5811805bb752c",
+    "url": "/motor-coach-direct/static/media/2019 american eagle heritage edition.254ef606.png"
+  },
+  {
+    "revision": "27755dae4a27f4e579756de1eee32b3e",
+    "url": "/motor-coach-direct/static/media/Force HD.27755dae.png"
+  },
+  {
+    "revision": "311c637a4fa46278063bd1e7fd002266",
+    "url": "/motor-coach-direct/static/media/DX3.311c637a.png"
+  },
+  {
+    "revision": "45ae5515f18ab2f1e7e775b83f6107e1",
+    "url": "/motor-coach-direct/static/media/dynaquest XL.45ae5515.png"
+  },
+  {
+    "revision": "f16a86e4de7233b09893e17c44cc752d",
+    "url": "/motor-coach-direct/static/media/2019 Entegra Insignia.f16a86e4.png"
+  },
+  {
+    "revision": "ab8faa7b03b390c704d4751986411841",
+    "url": "/motor-coach-direct/static/media/2019 Entegra Cornerstone.ab8faa7b.png"
+  },
+  {
+    "revision": "16fff317dd5cce84ed10352fa4ea125d",
+    "url": "/motor-coach-direct/static/media/2019 Entegra Aspire.16fff317.png"
+  },
+  {
+    "revision": "2907b0b6e34971556c5962e98c798896",
+    "url": "/motor-coach-direct/static/media/2019 Entegra Anthem.2907b0b6.png"
+  },
+  {
+    "revision": "1c39ce348d294f4a91626e2de0dd9f74",
+    "url": "/motor-coach-direct/static/media/2019 Entegra Vision.1c39ce34.png"
+  },
+  {
+    "revision": "253be50676d276281f5ddcb455d40cf2",
+    "url": "/motor-coach-direct/static/media/2019 Entegra Emblem.253be506.png"
+  },
+  {
+    "revision": "8cb45daff5ed6bf63b88",
+    "url": "/motor-coach-direct/static/js/main.8cb45daf.chunk.js"
+  },
+  {
+    "revision": "240901f4c27a0b447bfbd1f2c1087b9c",
+    "url": "/motor-coach-direct/static/media/2019 Fleetwood Discovery.240901f4.png"
+  },
+  {
+    "revision": "f3e149b4ced753417b7b98c957fd746f",
+    "url": "/motor-coach-direct/static/media/2019 Fleetwood Discovery LXE.f3e149b4.png"
+  },
+  {
+    "revision": "e7affef660731640dad5a9c391380c7c",
+    "url": "/motor-coach-direct/static/media/2019 Fleetwood Pace Arrow LXE.e7affef6.png"
+  },
+  {
+    "revision": "d94926859b01ff3a3fc503f8dd4102d7",
+    "url": "/motor-coach-direct/static/media/2019 Fleetwood Bounder.d9492685.png"
+  },
+  {
+    "revision": "8d189cc8e48547db461d574096b263c9",
+    "url": "/motor-coach-direct/static/media/2019 Fleetwood Southwind.8d189cc8.png"
+  },
+  {
+    "revision": "14c425c5abfb5c8fd8cd5ea6473d3d73",
+    "url": "/motor-coach-direct/static/media/2019 Monaco Signature.14c425c5.png"
+  },
+  {
+    "revision": "d711145cc694bc580c120da17a097ed3",
+    "url": "/motor-coach-direct/static/media/2019 Monaco Marquis.d711145c.png"
+  },
+  {
+    "revision": "084872bf12f04c92f244d5fdc7b1dfea",
+    "url": "/motor-coach-direct/static/media/2019 Newmar Dutch Star.084872bf.png"
+  },
+  {
+    "revision": "b54678532ecb18c10291c5daa3e28f17",
+    "url": "/motor-coach-direct/static/media/2019 Newmar Essex.b5467853.png"
+  },
+  {
+    "revision": "a5394f8bb9bb612728a9748144b0d355",
+    "url": "/motor-coach-direct/static/media/2019 Newmar King Aire.a5394f8b.png"
+  },
+  {
+    "revision": "64aaf9a5f8aafe26a8b22dfc50cab79e",
+    "url": "/motor-coach-direct/static/media/2019 Newmar London Aire.64aaf9a5.png"
+  },
+  {
+    "revision": "15eae3c5cf3d5e157be7e420d6d7dd70",
+    "url": "/motor-coach-direct/static/media/2019 Newmar Mountain Aire.15eae3c5.png"
+  },
+  {
+    "revision": "dfce9c941ef3575c3d3b8557d2bbf81a",
+    "url": "/motor-coach-direct/static/media/2019 Newmar New Aire.dfce9c94.png"
+  },
+  {
+    "revision": "b2d4cef3d0a30e491946ef200b68d438",
+    "url": "/motor-coach-direct/static/media/2019 Newmar Ventana.b2d4cef3.png"
+  },
+  {
+    "revision": "f19f7d377c93c6fb354f121bc2d884fa",
+    "url": "/motor-coach-direct/static/media/2019 Newmar Ventana LE.f19f7d37.png"
+  },
+  {
+    "revision": "1abeb630ac08c5a068c89ae2cef9c0db",
+    "url": "/motor-coach-direct/static/media/2019 Newmar Bay Star.1abeb630.png"
+  },
+  {
+    "revision": "933066a22500fb1cabdfaec172d15213",
+    "url": "/motor-coach-direct/static/media/2019 Newmar Canyon Star.933066a2.png"
+  },
+  {
+    "revision": "f390eea71ce807ea67593c0e3d673948",
+    "url": "/motor-coach-direct/static/media/2019 Tiffin Allegro Breeze.f390eea7.png"
+  },
+  {
+    "revision": "4b12d0b62ad35d084725fbf8a3539af3",
+    "url": "/motor-coach-direct/static/media/2019 Tiffin Allegro Bus.4b12d0b6.png"
+  },
+  {
+    "revision": "b49d8199ef8d49bc1da187985af8699d",
+    "url": "/motor-coach-direct/static/media/2019 Tiffin Allegro Red.b49d8199.png"
+  },
+  {
+    "revision": "be97bc90d26af6f319b2f70570ea3d55",
+    "url": "/motor-coach-direct/static/media/2019 Tiffin Open Road Allegro.be97bc90.png"
+  },
+  {
+    "revision": "f612ea0b4b423e596dd125eeb24808da",
+    "url": "/motor-coach-direct/static/media/2019 Tiffin Phaeton.f612ea0b.png"
+  },
+  {
+    "revision": "99556f8f3c80b6bf582e31ff780730bd",
+    "url": "/motor-coach-direct/static/media/2019 Tiffin Zephyr.99556f8f.png"
+  },
+  {
+    "revision": "a8b373b16f3f397b5adcc91b560b247f",
+    "url": "/motor-coach-direct/static/media/2019 Winnebago Forza.a8b373b1.png"
+  },
+  {
+    "revision": "fcc8bbc52499d993d5969dab74fc2351",
+    "url": "/motor-coach-direct/static/media/2019 Winnebago Grand Tour.fcc8bbc5.png"
+  },
+  {
+    "revision": "8acca64569f2bc50ab0879cd91cf2b9c",
+    "url": "/motor-coach-direct/static/media/2019 Winnebago Horizon.8acca645.png"
+  },
+  {
+    "revision": "c104fd18d0146bfac8a21f8d8d923d80",
+    "url": "/motor-coach-direct/static/media/2019 Winnebago Adventurer.c104fd18.png"
+  },
+  {
+    "revision": "5b2ec3d07a8808e545e7b29b8b932a10",
+    "url": "/motor-coach-direct/static/media/2019 Winnebago Vista.5b2ec3d0.png"
+  },
+  {
+    "revision": "7438dd8ce1d99b3656bc5c6918c4f3c1",
+    "url": "/motor-coach-direct/static/media/2019 Winnebago Sightseer.7438dd8c.png"
+  },
+  {
+    "revision": "6d6f77c2792fce2745410f4dade7c5b5",
+    "url": "/motor-coach-direct/static/media/2019 Winnebago Sunova.6d6f77c2.png"
+  },
+  {
+    "revision": "d44ddc03ec54b2e24e3d4a3de1200b3d",
+    "url": "/motor-coach-direct/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/motor-coach-direct/precache-manifest.accffc53b83c34f9cc64e0bb10176194.js"
+  "/motor-coach-direct/precache-manifest.a52d7fd4fc4165cde2d8de87acf74f21.js"
 );
 
 workbox.clientsClaim();
